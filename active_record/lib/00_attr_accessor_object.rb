ce3546b2372fe6ec17903e0273fb67c44f8a0b05
Kumasaka <anthonykumasaka@gmail.com>
2018-11-09 16:49:11 -0800

    Add exercise Active Record

---
+++ active_record/lib/00_attr_accessor_object.rb
@@ -0,0 +1,28 @@
+# In the lib/00_attr_accessor_object.rb file, implement a
+# ::my_attr_accessor macro, which should do exactly the same thing as
+#   the real attr_accessor: it should define setter/getter methods.
+#
+# To do this, use define_method inside ::my_attr_accessor to define
+#   getter and setter instance methods. You'll want to investigate
+#   and use the instance_variable_get and instance_variable_set
+#   methods described here.
+#
+# There is a corresponding spec/00_attr_accessor_object_spec.rb spec
+# file. Run it using bundle exec rspec to check your work.
+
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    # ...
+    names.each do |name|
+      define_method(name) do
+        self.instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |value|
+        self.instance_variable_set("@#{name}", value)
+      end
+    end
+    # instance_variable_get
+    # instance_variable_set
+  end
+end
